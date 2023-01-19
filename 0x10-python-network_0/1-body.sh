e2da9c58a93883a901caf28da5539fb3e8af1a18
Wendo324 <githakawendo@students.uonbi.ac.ke>
2023-01-19 12:11:48 -0800

    task2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
+# send a GET request to an URL with curl, and display the body of the response
 curl -sL "$1"
