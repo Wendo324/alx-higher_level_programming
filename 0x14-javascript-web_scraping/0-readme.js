f22af66680d4e7f6c0fef8f35b66d3281115d56d
Wendo324 <githakawendo@students.uonbi.ac.ke>
2023-02-09 20:02:41 +0300

    task1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
+const file = process.argv[2];
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
